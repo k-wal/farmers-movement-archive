672bdd53914b9521a7ca3cb909b208e56941370b
anshita <stellaolive157@gmail.com>
2021-01-08 22:56:07 +0530

    adding more code for graph creation for months and a few graphs

--- code/get_stats.py
+++ code/get_stats.py
@@ -19,8 +19,8 @@ def get_total_articles(dir_path):
 		total += get_day_article_total(filename)
 	return total
 
-# number of times keyword occurs in date in filename
-def get_keyword_count_day(filename, keyword):
+# number of times any of keywords occurs in date in filename
+def get_keyword_count_day(filename, keywords):
 	file = open(filename, 'r')
 	lines = file.readlines()
 	file.close()
@@ -29,8 +29,12 @@ def get_keyword_count_day(filename, keyword):
 	for line in lines:
 		parts = line.split('||')
 		date, location, title, text = parts[0], parts[1], parts[2], parts[3]
-		if keyword in title.lower() or keyword in text.lower():
-			count += 1
+		
+		# if any one keyword is present, increase count
+		for keyword in keywords:
+			if keyword in title.lower() or keyword in text.lower():
+				count += 1
+				break
 
 	return count
 
@@ -73,10 +77,10 @@ def create_keywords_graph(counts):
 	count_list = [x[0] for x in counts]
 	month_list = []
 	tick_locs = []
-	remainder = 0
+	remainder = 30
 	for i,count in enumerate(counts):
 		# show every nth date
-		if remainder == 35:
+		if remainder == 30:
 			month_list.append(count[1] + '/' + count[2] + '/' + count[3][2:4])
 			tick_locs.append(i)
 			remainder = 0
@@ -91,6 +95,18 @@ def create_keywords_graph(counts):
 	plt.legend()
 	plt.show()
 
+
+def create_keywords_monthly_graph(counts):
+	count_list = [x[0] for x in counts]
+	month_list = [(str(x[1]) + '/' + str(x[2])) for x in counts]
+	plt.plot(month_list, count_list, 'r', label = '#articles per month with ' + ', '.join(keywords))
+	plt.title('keywords : ' + ', '.join(keywords))
+	plt.ylabel('number of articles')
+	plt.xlabel('month')
+	plt.legend()
+	plt.show()
+
+
 # givend path to corpus and list of keywords, create a graph of number of occurences vs time
 def get_keywords_timeline(dir_path, keywords):
 	counts = []
@@ -102,29 +118,52 @@ def get_keywords_timeline(dir_path, keywords):
 	create_keywords_graph(counts)
 
 
+# givend path to corpus and list of keywords, create a graph of number of occurences vs time
+def get_keywords_monthly_timeline(dir_path, keywords):
+	counts = []
+	files = os.listdir(dir_path)
+	for file in files:
+		filename = dir_path + '/' + file
+		counts.append(get_keywords_count_with_date(filename, keywords))
+	monthly_counts = {}
+
+	for count in counts:
+		key = str(count[2]) + '/' + str(count[3])
+		if key not in monthly_counts.keys():
+			monthly_counts[key] = [0, count[2], count[3]]
+		monthly_counts[key][0] += count[0]
+
+	monthly_counts = sorted(monthly_counts.values(), key=operator.itemgetter(2,1)) # sorting according to date
+	create_keywords_monthly_graph(monthly_counts)
+
+
+
+
 corpus_path = '../corpus/hindu'
 
 total_articles = get_total_articles(corpus_path)
 print('total number of articles : ' + str(total_articles))
 
-keyword = 'farmer'
+keyword = ['farmer','msp']
 total_keyword_articles = get_keyword_counts_total(corpus_path, keyword)
-print('total articles with keyword ' + keyword + ' in them  : ' + str(total_keyword_articles))
+print('total articles with keyword ' + ', '.join(keyword) + ' in them  : ' + str(total_keyword_articles))
 
-keyword = 'msp'
+keyword = ['farmer','esca','apmc']
 total_keyword_articles = get_keyword_counts_total(corpus_path, keyword)
-print('total articles with keyword ' + keyword + ' in them  : ' + str(total_keyword_articles))
+print('total articles with keyword ' + ', '.join(keyword) + ' in them  : ' + str(total_keyword_articles))
 
-keyword = 'apmc'
+keyword = ['apmc']
 total_keyword_articles = get_keyword_counts_total(corpus_path, keyword)
-print('total articles with keyword ' + keyword + ' in them  : ' + str(total_keyword_articles))
+print('total articles with keyword ' + ', '.join(keyword) + ' in them  : ' + str(total_keyword_articles))
 
-keyword = 'esca'
+keyword = ['esca']
 total_keyword_articles = get_keyword_counts_total(corpus_path, keyword)
-print('total articles with keyword ' + keyword + ' in them  : ' + str(total_keyword_articles))
+print('total articles with keyword ' + ', '.join(keyword) + ' in them  : ' + str(total_keyword_articles))
 
 
 # GRAPH
-keywords = ['farmer']
-get_keywords_timeline(corpus_path, keywords)
+keywords = ['farmer','esca','apmc']
+# get_keywords_timeline(corpus_path, keywords)
+# keywords = ['farmer']
+get_keywords_monthly_timeline(corpus_path, keywords)
 
