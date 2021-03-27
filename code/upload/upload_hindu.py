e9b9e9951130cac2f82c42f9e9118a6f9d1c58ba
anshita <stellaolive157@gmail.com>
2021-03-27 23:06:39 +0530

    commiting latest changes

--- code/upload/upload_hindu.py
+++ code/upload/upload_hindu.py
@@ -8,20 +8,41 @@ def get_location(part):
 	return part 
 
 
-item_set_dict = {'hindu' : 330}
+item_set_dict = {12 : 327,
+11 : 7348,
+10 : 7454,
+9 : 7972,
+8 : 8335,
+7 : 8495,
+6 : 9297,
+5 : 9298,
+4 : 9299}
 
-keywords = ['farmer',
-'mandi',
-'agrarian crisis',
+keywords = [
+# 'farmer',
+# 'mandi',
+# 'agrarian crisis',
 'kisan sabha',
-'msp',
+#'msp',
 'bku',
 'tikri', 
 'singhu', 
-'ghazipur',
+# 'ghazipur',
+'anti-farmer',
+'agri-reform',
 'farm bill',
 'farm bills',
-'the essential commodities',
+'farmers bills',
+'farmers\' bills',
+'farm policy',
+'farm policies',
+'pro-farmer',
+'Essential Commodities (Amendment) Bill, 2020',
+'Essential Commodities Bill, 2020',
+'Essential Commodities Act, 2020',
+'agri bill',
+'agri ordinance',
+'farm ordinance',
 'trolley times',
 'Kisan Sangharsh Committee',
 'Kisan Bachao Morcha',
@@ -29,12 +50,13 @@ keywords = ['farmer',
 'Jai Kisan Andolan',
 'Punjab Kisan Union',
 'Kirti Kisan Union',
-'Terai Kisan Sangatha',
+'Terai Kisan Sangathan',
 'All India Kisan Sabha',
 'Mahila Kisan Adhikar Manch',
 'Doaba Kisan Samiti',
 'Rakesh Tikait',
 'Bhartiya Kisan Union']
 
+month = 5
 
-upload_section('../../corpus/hindu', item_set_dict['hindu'], 'The Hindu', '', keywords, get_location)
+upload_section('../../corpus/hindu/' + str(month), item_set_dict[month], 'The Hindu', '', keywords, get_location)
