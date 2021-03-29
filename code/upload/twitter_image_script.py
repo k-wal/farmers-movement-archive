157cefc18680ea602147fda92c594c0c94019fa6
anshita <stellaolive157@gmail.com>
2021-03-30 00:56:12 +0530

    uploading picture with exisiting twitters

--- code/upload/twitter_image_script.py
+++ code/upload/twitter_image_script.py
@@ -23,15 +23,24 @@ def get_image_url(url, date, username):
 	c.Store_object = True
 	c.Format = "{photos} || {video}"
 	c.Limit = 1
-	twint.run.Search(c)
-	tweets_as_objects = twint.output.tweets_list 
+	try:
+		twint.run.Search(c)
+	except:
+		return []
+	tweets_as_objects = twint.output.tweets_list
+	twint.output.tweets_list = [] 
+	if len(tweets_as_objects) == 0:
+		print(url, date, username)
+		return []
 	return tweets_as_objects[0].photos
 
 # return a photo path if successfully downloaded, else return empty string
 def store_image(url):
+	print(url)
 	extension = url.split('.')[-1]
 	r = requests.get(url, stream=True)
 	if r.status_code != 200:
+		print("xxxxxxxxxxx")
 		return ''
 	r.raw.decode_content = True
 	with open('twitter_image.' + extension, 'wb') as f:
@@ -58,8 +67,7 @@ def if_image(text, date, username):
 
 
 
-# url = 'https://t.co/hUTCGs6nRM'
-# print(get_image_url(url))
-text = ' प्रधान मंत्री नरेंद्र मोदी ने आज केंद्र के नए कृषि कानूनों और उनके द्वारा लाए गए फायदों की बात की, क्योंकि देश में पिछले चार दिनों से किसानों द्वारा अभूतपूर्व विरोध प्रदर्शन हो रहे थे।   #preminister #modi #farmer #farmersprotest #farmbill #opportunity  https://t.co/hUTCGs6nRM'
-date = '2020-11-29'
-username = 'VidharbhS'
+# text = ' प्रधान मंत्री नरेंद्र मोदी ने आज केंद्र के नए कृषि कानूनों और उनके द्वारा लाए गए फायदों की बात की, क्योंकि देश में पिछले चार दिनों से किसानों द्वारा अभूतपूर्व विरोध प्रदर्शन हो रहे थे।   #preminister #modi #farmer #farmersprotest #farmbill #opportunity  https://t.co/hUTCGs6nRM'
+# date = '2020-11-29'
+# username = 'VidharbhS'
+# print(if_image(text, date, username))
--- code/upload/upload_tweets.py
+++ code/upload/upload_tweets.py
@@ -6,6 +6,7 @@ import json
 import datetime
 import numpy as np
 import pickle
+import twitter_image_script as image_script
 
 params = {
     'key_identity': 'Xz9kFHaoaCaCKIz9Ez2UNjgBkLxLmZqg',
@@ -13,7 +14,9 @@ params = {
 }
 
 item_set_id = {
-11:9314
+11 : 9314,
+8 : 12266,
+10 : 12327
 }
 
 def upload_article(date, item_set_id, title, description, url, username, user_url, tweet_id):
@@ -33,19 +36,75 @@ def upload_article(date, item_set_id, title, description, url, username, user_ur
 		#"dcterms:coverage" : [{"property_id" : 14, "property_label" : "Coverage", "@value" : location, "type" : "literal"}],
 		"o:resource_class" : {"o:id" : 72} ,
 		"@type" : "o:Item",
-		"o:item_set" : [ {"o:id": item_set_id}], 
+		"o:item_set" : [ {"o:id": item_set_id}, {"o:id" : 9315}], 
 		"o:media" : []
 	}
 
 	response = requests.post('http://indiasocialarchive.iiit.ac.in/api/items', headers=headers, params=params, data=json.dumps(data))
 	j = json.loads(response.text)
+	return j['@id'].split('/')[-1]
 	#print(j)
 
+def upload_article_file(item_id, photo_path):
+	headers = {
+	'Content-Type': 'multipart/form-data'
+	}
+
+
+	data = {"o:ingester" : "upload", 
+			"file_index" : "0", 
+			"o:item" : {"o:id" : item_id}
+			}
+
+	files = [('data',(None, json.dumps(data), 'application/json')),
+				('file[0]', (photo_path, open(photo_path, 'rb'), 'image/jpg'))]
+
+	response = requests.post('http://indiasocialarchive.iiit.ac.in/api/media', params=params, files=files)
+	print(response)
+
+def embed_article(date, item_set_id, title, description, url, username, user_url, tweet_id):
+	r = requests.get('https://publish.twitter.com/oembed?url=' + url)
+	r = r.json()
+
+	headers = {
+	'Content-Type': 'application/json'
+	}
+
+
+	media_data = {"o:ingester" : "oembed",
+			"o:renderer" : "html",  
+			"o:source" : url,
+			}
+
+	files = [('data',(None, json.dumps(r), 'application/json'))]
+
+	data = {
+		"dcterms:title" : [{"property_id" : 1, "property_label" : "Title", "@value" : title, "type" : "literal"}],
+		"dcterms:description" : [{"property_id" : 4, "property_label" : "Description", "@value" : description, "type" : "literal"}],
+		"dcterms:source" : [{"property_id" : 11, "property_label" : "Source", "@id" : url, "type" : "uri"}],
+		#"dcterms:publisher" : [{"property_id" : 5, "property_label" : "Publisher", "@value" : publisher, "type" : "literal"}],
+		"dcterms:date" : [{"property_id" : 7, "property_label" : "Date", "@value" : date, "type" : "literal"}],
+		"dcterms:contributor" : [{"property_id" : 6, "property_label" : "Contributor", "o:label" : username, "@id" : user_url, "type" : "uri"}],
+		"dcterms:identifier" : [{"property_id" : 10, "property_label" : "Identifier", "@value" : tweet_id, "type" : "literal"}],
+		#"dcterms:coverage" : [{"property_id" : 14, "property_label" : "Coverage", "@value" : location, "type" : "literal"}],
+		"o:resource_class" : {"o:id" : 72} ,
+		"@type" : "o:Item",
+		"o:item_set" : [ {"o:id": item_set_id}, {"o:id" : 9315}], 
+		"o:media" : [media_data]
+	}
+
+
+	response = requests.post('http://indiasocialarchive.iiit.ac.in/api/media', params=params, data=data, files=files)
+	print(response)
 
 def if_upload(date, tweet_id):
 	tweet_id = int(tweet_id)
-	month = date.split('-')[1]
-	dir_path = 'ids/'
+	month = date.split('-')[1] + '-' + date.split('-')[0]
+	dir_path = 'ids/' + month + '/'
+
+	if not os.path.exists(dir_path):
+		os.mkdir(dir_path)
+
 	filenames = os.listdir(dir_path)
 	max_per_file = 5000
 
@@ -86,14 +145,21 @@ def upload_file(filepath, item_set_id):
 		parts = line.split('|-|')
 		date, tweet_id, username, text, url = parts[0].strip(), parts[1].strip(), parts[3].strip(), parts[7].strip(), parts[8].strip()
 	
-		if if_upload(date,tweet_id):
-			user_url = url.split('/status/')[0]
-			if len(text) > 30:
-				desc = text[0:30] + " . . . . "
-			else:
-				desc = text
-			title = "<em>" + username + "</em> : " + desc
-			upload_article(date, item_set_id, title, text, url, username, user_url, tweet_id)
+		if not if_upload(date,tweet_id):
+			continue
+		user_url = url.split('/status/')[0]
+		if len(text) > 30:
+			desc = text[0:30] + " . . . . "
+		else:
+			desc = text
+		title = "<em>" + username + "</em> : " + desc
+
+		photo_path = image_script.if_image(text, date, username)
+		item_id = upload_article(date, item_set_id, title, text, url, username, user_url, tweet_id)
+		if photo_path != '':
+			upload_article_file(item_id, photo_path)
+		# embed_article(date, item_set_id, title, text, url, username, user_url, tweet_id)
+
 		if index % 10 == 0:
 			print(str(index) + " of " + str(total) + " done")
 
@@ -106,5 +172,5 @@ def upload_section(dir_path, item_set_id):
 		upload_file(path, item_set_id)
 		print(filename + " : end")
 
-filepath = '../../corpus/temp_tweets/farmbill/11-2020.txt'
-upload_file(filepath, item_set_id[11])
+filepath = '../../corpus/temp_tweets/farmbill/08-2020.txt'
+upload_file(filepath, item_set_id[8])
