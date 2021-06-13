877ed7f18e27937dead92a8acc11a800fb36555d
anshita <stellaolive157@gmail.com>
2021-06-13 22:16:23 +0530

    adding HT combine script and basic upload script(needs work for interval upload and scraping and to add function in updater.py), adding combined files for april-2020 to may-2021

---
+++ code/remove_repetition/ht_combiner.py
@@ -0,0 +1,51 @@
+import os
+import re
+import datetime
+
+def combine_date(date, dir_path):
+	titles = []
+	articles = []
+
+	month = datetime.datetime.strftime(date, "%m-%Y")
+	dir_path = dir_path + '/' + month
+	filename = datetime.datetime.strftime(date, "%d-%m-%Y.txt")
+
+	out_dir_path = dir_path + '/combined'
+	if not os.path.exists(out_dir_path):
+		os.makedirs(out_dir_path)
+	outfile = open(out_dir_path + '/' + filename, 'w')
+
+	for section in os.listdir(dir_path):
+		if section == 'combined':
+			continue
+		filepath = dir_path + '/' + section + '/' + filename
+		if filename not in os.listdir(dir_path + '/' + section):
+			continue
+
+		file = open(filepath, 'r')
+		lines = file.readlines()
+		file.close()
+
+		for line in lines:
+			title = line.split('||')[2]
+			if title in titles:
+				continue
+			outfile.write(line)
+			titles.append(title)
+	outfile.close()
+
+
+def combine_interval(start_string, end_string, dir_path):
+	start_date = datetime.datetime.strptime(start_string, "%d-%m-%Y")
+	end_date = datetime.datetime.strptime(end_string, "%d-%m-%Y")
+	date = start_date
+	while date <= end_date:
+		print(date)
+		combine_date(date, dir_path)
+		date += datetime.timedelta(days=1)
+
+
+start_string = "01-04-2020"
+end_string = "31-05-2021"
+dir_path = '../../corpus/hindustantimes'
+combine_interval(start_string, end_string, dir_path)
